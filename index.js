04be08b351fe2c37059d60bb0617ba52e27a98da
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-01-01 00:09:43 +0600

    Starter file for node server

---
+++ index.js
@@ -0,0 +1,23 @@
+// Main starting point for our app
+const express = require("express"),
+  http = require("http"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  app = express(),
+  router = require("./router"),
+  mongoose = require("mongoose")
+
+// DB setup
+mongoose.connect("mongodb://localhost:27017/auth")
+
+// App setup with express middlewares
+app.use(morgan("combined"))
+app.use(bodyParser({ type: "*/*" }))
+router(app)
+
+// Server setup
+const port = process.env.PORT || 3090,
+  server = http.createServer(app)
+
+server.listen(port)
+console.log("Server listening to:", port)
