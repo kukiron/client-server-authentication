b4a1147beecde0a516dbf3b250cd9df88c1e1de1
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2018-01-15 01:22:54 +0600

    Server side authentication

---
+++ server/index.js
@@ -0,0 +1,25 @@
+// Main starting point for our app
+const express = require("express"),
+  http = require("http"),
+  bodyParser = require("body-parser"),
+  morgan = require("morgan"),
+  app = express(),
+  router = require("./router"),
+  mongoose = require("mongoose"),
+  cors = require("cors")
+
+// DB setup
+mongoose.connect("mongodb://localhost:27017/auth", { useMongoClient: true })
+
+// App setup with express middlewares
+app.use(morgan("combined"))
+app.use(cors())
+app.use(bodyParser({ type: "*/*" }))
+router(app)
+
+// Server setup
+const port = process.env.PORT || 3090,
+  server = http.createServer(app)
+
+server.listen(port)
+console.log("Server listening to:", port)
